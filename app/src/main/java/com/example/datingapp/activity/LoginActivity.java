acc0060bfef613248f04e0252d7e8d23995eb91b
pthngws <pthngws@gmail.com>
2025-03-17 16:13:26 +0700

    update LoginActivity

--- app/src/main/java/com/example/datingapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/datingapp/activity/LoginActivity.java
@@ -2,9 +2,9 @@ package com.example.datingapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,4 +13,33 @@ import com.example.datingapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText emailInput, passwordInput;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        emailInput = findViewById(R.id.emailInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailInput.getText().toString().trim();
+                String password = passwordInput.getText().toString().trim();
+
+                if (email.equals("1") && password.equals("1")) {
+                    Toast.makeText(LoginActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Email hoặc mật khẩu không đúng!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 }
